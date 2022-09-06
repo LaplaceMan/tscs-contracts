0c08a790038611c7eec1d70683d8c757fa34ff10
LaplaceMan <505876833@qq.com>
2022-09-06 21:41:50 +0800

    9.16

--- contracts/base/SubtitleManager.sol
+++ contracts/base/SubtitleManager.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SubtitleManager {
+import "../common/token/ERC721/ERC721.sol";
+
+contract SubtitleManager is ERC721 {
+    
+    mapping(uint256 => Subtitle) subtitleNFT;
+    
     struct Subtitle {
         address platfrom;
         string language;
-        string ipfs;
+        uint8 state;
+        uint256 support;
+        uint256 opposition;
+        address[] supporters;
+        address[] protecter;
     }
+
+
 }
